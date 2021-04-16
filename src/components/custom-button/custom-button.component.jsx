3c6443ba0442e21881c9a5796d8aa31f95f449f9
Pascal-Eric Servais <anynamus@yahoo.ca>
2021-04-16 04:39:36 -0400

    moved the google sign in button type at the right place.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -5,7 +5,6 @@ import './custom-button.styles.scss';
 const CustomButtom = ({ children, isGoogleSignIn, ...otherProps }) => (
     <button
         className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
-        type='button'
         {...otherProps}
     >
         {children}
